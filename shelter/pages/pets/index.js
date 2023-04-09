15b2a1c69c6ebcc4373b56c28f8b75d81a19e137
Nastya <nkartsel@gmail.com>
2023-04-09 19:05:17 +0300

    fix: burger function

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -2,18 +2,16 @@
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header-nav');
-    const menuCloseItem = document.querySelector('.header-nav-close');
     const overlay = document.querySelector('.overlay');
     const header = document.querySelector('.header');
+
+    
     burgerItem.addEventListener('click', () => {
-        menu.classList.add('header-nav-active');
-        overlay.classList.add('overlay-active');
-        header.classList.add('header-overlay-active');
-        
+        menu.classList.toggle('header-nav-active');
+        overlay.classList.toggle('overlay-active');
+        header.classList.toggle('header-overlay-active');
+        burgerItem.classList.toggle('header-burger-active'); //
+       
     });
-    menuCloseItem.addEventListener('click', () => {
-        menu.classList.remove('header-nav-active');
-        overlay.classList.remove('overlay-active');
-        header.classList.remove('header-overlay-active');
-    });
+
 }());
