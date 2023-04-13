16a8078d2bd9a143c70512582663847eaf4c9be0
Nastya <nkartsel@gmail.com>
2023-04-13 16:01:23 +0300

    feat: add overflow hidden

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -2,12 +2,13 @@
 (function () {
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header-nav');
-    
+    const html = document.querySelector('html');
     const overlay = document.querySelector('.overlay');
     burgerItem.addEventListener('click', () => {
         menu.classList.toggle('header-nav-active');
         overlay.classList.toggle('overlay-active');
         burgerItem.classList.toggle('header-burger-active');
+        html.classList.toggle('html-scroll');
     });
   
 }());
--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -4,14 +4,14 @@
     const menu = document.querySelector('.header-nav');
     const overlay = document.querySelector('.overlay');
     const header = document.querySelector('.header');
-
+    const html = document.querySelector('html');
     
     burgerItem.addEventListener('click', () => {
         menu.classList.toggle('header-nav-active');
         overlay.classList.toggle('overlay-active');
         header.classList.toggle('header-overlay-active');
-        burgerItem.classList.toggle('header-burger-active'); //
-       
+        burgerItem.classList.toggle('header-burger-active');
+        html.classList.toggle('html-scroll');
     });
 
 }());
